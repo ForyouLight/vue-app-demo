7988bea6f81434eafd050fcf62ba3a4bf7953d69
Steven <2724847566@qq.com>
2019-05-29 12:23:45 +0800

    主页

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,17 @@
 <template>
   <div id="app">
-    <a-menu
-    mode="horizontal">
-      <a-menu-item key="mainMenu">首页</a-menu-item>
-      <a-sub-menu title="子菜单">
-        <a-menu-item>子菜单项</a-menu-item>
-      </a-sub-menu>
-    </a-menu>
-    <img src="./assets/logo.png">
+    <app-menu></app-menu>
+    <router-view/>
   </div>
 </template>
 
 <script>
+import MenuVue from './components/Menu.vue';
 export default {
-  name: 'App'
+  name: 'App',
+  components:{
+    "app-menu":MenuVue,
+  }
 }
 </script>
 
@@ -22,8 +20,6 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
- 
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
