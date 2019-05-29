74a77ce43bcdbd671d9a0067304f8b9df554800c
Steven <2724847566@qq.com>
2019-05-29 12:24:38 +0800

    菜单

---
+++ src/components/Menu.vue
@@ -0,0 +1,34 @@
+<template>
+    <div id="menu">
+        <div class="logo">Peony OS</div>
+        <a-menu mode="horizontal"
+        defaultSelectedKeys="1">
+            <a-menu-item key="1" ><router-link to="/">首页</router-link></a-menu-item>
+            <a-menu-item key="2">首页</a-menu-item>
+            <a-sub-menu title="子菜单">
+            <a-menu-item>子菜单项</a-menu-item>
+        </a-sub-menu>
+        </a-menu>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+
+<style  scoped>
+#menu{
+    width: 100%;
+    position: fixed;
+}
+.logo{
+    width: 150px;
+    height: 49px;
+    float: left;
+    font-size: 25px;
+    text-align: center;
+    line-height: 50px;
+}
+</style>
+
